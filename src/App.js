2a74aeb5fbad4ccc94726d678d4c6875dadd411f
Heesun <heesun729@naver.com>
2024-06-17 17:46:28 +0900

    ✨feat: languageType, 아바타, 말풍선 추가

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/global-styles';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SelectLanguage from './pages/SelectLanguage';
+import SelectStage from './pages/SelectStage';
+import CorrectAnswer from './pages/CorrectAnswer';
 import theme from './styles/theme';
 import Start from './pages/Start';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [cntLife, setCntLife] = useState(5); // 초기 목숨 5개
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <BrowserRouter>
-         <Routes>
-          <Route path="/selectLanguage" element={<SelectLanguage />} />\
-          <Route path="/" element={<Start />} />     
-         <Route
+        <Routes>
+          <Route path="/" element={<Start />} />
+          <Route
             path="/selectLanguage"
             element={
               <SelectLanguage
@@ -26,6 +28,16 @@ function App() {
             }
           />
           <Route path="selectStage" element={<SelectStage />} />
+          <Route
+            path="/correctAnswer"
+            element={
+              <CorrectAnswer
+                isLoggedIn={isLoggedIn}
+                setIsLoggedIn={setIsLoggedIn}
+                cntLife={cntLife}
+              />
+            }
+          />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
--- src/pages/SelectLanguage.js
+++ src/pages/SelectLanguage.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled, { keyframes } from 'styled-components';
 import Layout from '../components/Layout';
 import PropTypes from 'prop-types';
 import Main from '../components/Main';
 import Header from '../components/Header';
-import { Name, Strong, LanguageButton } from '../components/styled';
+import { Name, Strong, LanguageButton } from '../styles/styled';
+import AvatarImg from '../assets/avatar.png';
+import sayImg from '../assets/say.png';
 
 function SelectLanguage({ isLoggedIn, setIsLoggedIn }) {
+  const [languageType, setLanguageType] = useState('');
+
+  const handleLanguageSelection = (language) => {
+    setLanguageType(language);
+  };
+
   return (
     <Main.Wrapper>
       <Header isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
@@ -13,8 +22,23 @@ function SelectLanguage({ isLoggedIn, setIsLoggedIn }) {
         <Name>
           사용할 <Strong>언어</Strong>를 선택해주세요
         </Name>
-        <LanguageButton>JAVA</LanguageButton>
-        <LanguageButton>JAVASCRIPT</LanguageButton>
+        <LanguageButton onClick={() => handleLanguageSelection('java')}>
+          JAVA
+        </LanguageButton>
+        <LanguageButton onClick={() => handleLanguageSelection('javascript')}>
+          JAVASCRIPT
+        </LanguageButton>
+        {languageType && (
+          <SpeechBubbleWrapper>
+            <Avatar src={AvatarImg} alt="avatarImg" />
+            <SpeechBubble>
+              <TextWrapper>
+                {languageType.toUpperCase()}로 모험 시작하기
+                <NextArrow>→</NextArrow>
+              </TextWrapper>
+            </SpeechBubble>
+          </SpeechBubbleWrapper>
+        )}
       </Layout.PageContent>
     </Main.Wrapper>
   );
@@ -24,5 +48,62 @@ SelectLanguage.propTypes = {
   isLoggedIn: PropTypes.bool.isRequired,
   setIsLoggedIn: PropTypes.func.isRequired,
 };
+
+const bounce = keyframes`
+  0%, 100% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-15px);
+  }
+`;
+
+const SpeechBubbleWrapper = styled.div`
+  display: flex;
+  align-items: flex-end;
+  margin-top: 25px;
+  position: relative;
+`;
+
+const Avatar = styled.img`
+  width: 90px;
+  height: 90px;
+  animation: ${bounce} 1s infinite;
+  filter: drop-shadow(0px 4px 8px rgba(0, 0, 0, 0.3));
+`;
+
+const SpeechBubble = styled.div`
+  background-image: url(${sayImg});
+  background-size: cover;
+  background-repeat: no-repeat;
+  padding: 10px 20px 10px 20px;
+  margin-left: 10px;
+  display: flex;
+  align-items: center;
+  width: 300px; /* 말풍선의 너비를 줄임 */
+  height: 60px; /* 말풍선의 높이를 줄임 */
+  text-align: center;
+  font-size: 14px; /* 텍스트 크기를 줄임 */
+  position: relative;
+  top: -10px;
+`;
+
+const TextWrapper = styled.div`
+  justify-content: space-between;
+  align-items: center;
+  margin: 5px 0px 0px 50px;
+  display: flex;
+  flex: 1; /* 남은 공간을 차지하게 설정 */
+`;
+const NextArrow = styled.div`
+  cursor: pointer;
+  font-size: 24px;
+  color: black;
+  font-weight: bold;
+  &:hover {
+    color: #007bff;
+    font-size: 26px;
+  }
+`;
 
 export default SelectLanguage;
