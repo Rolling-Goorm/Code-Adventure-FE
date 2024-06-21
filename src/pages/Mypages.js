053a0a48b65ba46bdc45a209308305678dfc4d35
Kimsumin123 <tntn211@naver.com>
2024-06-21 22:44:44 +0900

    마이 페이지 수정 기능 추가

--- src/pages/Mypages.js
+++ src/pages/Mypages.js
@@ -3,11 +3,12 @@ import { useNavigate } from 'react-router-dom';
 import Layout from '../components/Layout';
 import Main from '../components/Main';
 import { Name } from '../styles/styled';
-import { UserInfoItem, Button } from '../styles/LoginStyle';
+import { UserInfoItem, Button, Input } from '../styles/LoginStyle'; // Import Input and Button for styling
 import { AuthContext } from '../components/AuthContext';
 
-function Mypages(props) {
+function Mypages() {
   const [userInfo, setUserInfo] = useState(null);
+  const [editMode, setEditMode] = useState(false); // State to toggle edit mode
   const [editInfo, setEditInfo] = useState({
     loginId: '',
     loginPassword: '',
@@ -19,7 +20,7 @@ function Mypages(props) {
     phoneNumber: '',
   });
 
-  const { user } = useContext(AuthContext); // AuthContext에서 사용자 정보 가져오기
+  const { user } = useContext(AuthContext);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -27,7 +28,7 @@ function Mypages(props) {
       fetch(`http://118.67.128.223:8080/users/${user.id}`)
         .then((res) => res.json())
         .then((json) => {
-          console.log('User info fetched:', json); // 유저 정보 콘솔 출력
+          console.log('User info fetched:', json);
           setUserInfo(json);
           setEditInfo({
             loginId: json.username,
@@ -54,7 +55,7 @@ function Mypages(props) {
   };
 
   const handleSave = () => {
-    fetch(`http://localhost:8080/users/${userInfo.id}`, {
+    fetch(`http://118.67.128.223:8080/users/${user.id}`, {
       method: 'PATCH',
       headers: {
         'Content-Type': 'application/json',
@@ -64,6 +65,7 @@ function Mypages(props) {
       .then((res) => res.json())
       .then((updatedInfo) => {
         setUserInfo(updatedInfo);
+        setEditMode(false);
         alert('User info updated successfully');
       })
       .catch((error) => {
@@ -80,15 +82,80 @@ function Mypages(props) {
     <Main.Wrapper>
       <Layout.PageContent>
         <Name>마이페이지</Name>
-        <UserInfoItem>아이디: {userInfo.loginId}</UserInfoItem>
-        <UserInfoItem>이름: {userInfo.name}</UserInfoItem>
-        <UserInfoItem>닉네임: {userInfo.nickname}</UserInfoItem>
-        <UserInfoItem>선호 언어 : {userInfo.preferredLanguage}</UserInfoItem>
-        <UserInfoItem>생일 : {userInfo.birth}</UserInfoItem>
-        <UserInfoItem>이메일 : {userInfo.email}</UserInfoItem>
-        <UserInfoItem>전화번호 : {userInfo.phoneNumber}</UserInfoItem>
-        <Button onClick={handleSave}>저장</Button>
-        <Button onClick={() => navigate('/')}>메인으로 돌아가기</Button>
+        {editMode ? (
+          <>
+            <UserInfoItem>아이디</UserInfoItem>
+            <Input
+              type="text"
+              name="loginId"
+              value={editInfo.loginId}
+              onChange={handleChange}
+            />
+            <UserInfoItem>비밀번호</UserInfoItem>
+            <Input
+              type="password"
+              name="loginPassword"
+              value={editInfo.loginPassword}
+              onChange={handleChange}
+            />
+            <UserInfoItem>이름</UserInfoItem>
+            <Input
+              type="text"
+              name="name"
+              value={editInfo.name}
+              onChange={handleChange}
+            />
+            <UserInfoItem>닉네임</UserInfoItem>
+            <Input
+              type="text"
+              name="nickname"
+              value={editInfo.nickname}
+              onChange={handleChange}
+            />
+            <UserInfoItem>선호 언어</UserInfoItem>
+            <Input
+              type="text"
+              name="preferredLanguage"
+              value={editInfo.preferredLanguage}
+              onChange={handleChange}
+            />
+            <UserInfoItem>생일</UserInfoItem>
+            <Input
+              type="date"
+              name="birth"
+              value={editInfo.birth}
+              onChange={handleChange}
+            />
+            <UserInfoItem>이메일</UserInfoItem>
+            <Input
+              type="email"
+              name="email"
+              value={editInfo.email}
+              onChange={handleChange}
+            />
+            <UserInfoItem>전화번호</UserInfoItem>
+            <Input
+              type="tel"
+              name="phoneNumber"
+              value={editInfo.phoneNumber}
+              onChange={handleChange}
+            />
+            <Button onClick={handleSave}>저장</Button>
+            <Button onClick={() => setEditMode(false)}>취소</Button>
+          </>
+        ) : (
+          <>
+            <UserInfoItem>아이디: {userInfo.loginId}</UserInfoItem>
+            <UserInfoItem>이름: {userInfo.name}</UserInfoItem>
+            <UserInfoItem>닉네임: {userInfo.nickname}</UserInfoItem>
+            <UserInfoItem>선호 언어: {userInfo.preferredLanguage}</UserInfoItem>
+            <UserInfoItem>생일: {userInfo.birth}</UserInfoItem>
+            <UserInfoItem>이메일: {userInfo.email}</UserInfoItem>
+            <UserInfoItem>전화번호: {userInfo.phoneNumber}</UserInfoItem>
+            <Button onClick={() => setEditMode(true)}>수정</Button>
+            <Button onClick={() => navigate('/')}>메인으로 돌아가기</Button>
+          </>
+        )}
       </Layout.PageContent>
     </Main.Wrapper>
   );
